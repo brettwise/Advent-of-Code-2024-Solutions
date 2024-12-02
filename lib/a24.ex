2e4e052bded088c87e66f845e0e2d4679ec69a2f
Brett Wise <brett-gh@zmail.email>
2024-12-02 15:25:45 -0500

    Day 1 pt 1 & 2: working solutions

--- lib/a24.ex
+++ lib/a24.ex
@@ -1,18 +1,64 @@
 defmodule A24 do
   @moduledoc """
-  Documentation for `A24`.
+  Advent of Code 2024 solutions.
   """
+  alias A24.Inputs
 
-  @doc """
-  Hello world.
+  def day1pt2 do
+    Inputs.day1()
+    |> String.split("\n")
+    |> Enum.reduce(
+      [[], []],
+      fn line, [list_1_acc, list_2_acc] = separated_lists ->
+        if line != "" do
+          [list_1_item, list_2_item] =
+            line
+            |> String.split("   ", trim: true)
+            |> Enum.map(&String.to_integer/1)
 
-  ## Examples
+          [[list_1_item | list_1_acc], [list_2_item | list_2_acc]]
+        else
+          [list_1_sorted, list_2_sorted] =
+            Enum.map(separated_lists, &Enum.sort/1)
 
-      iex> A24.hello()
-      :world
+          # NOTE: this is the only diff b/w this & day 1
+          _total_similarity_score =
+            Enum.reduce(list_1_sorted, 0, fn id, similarity_score ->
+              left_num_in_right_list_count =
+                list_2_sorted
+                |> Enum.filter(fn list_2_item -> id == list_2_item end)
+                |> Enum.count()
 
-  """
-  def hello do
-    :world
+              similarity_score + id * left_num_in_right_list_count
+            end)
+        end
+      end
+    )
+  end
+
+  def day1 do
+    Inputs.day1()
+    |> String.split("\n")
+    |> Enum.reduce(
+      [[], []],
+      fn line, [list_1_acc, list_2_acc] = separated_lists ->
+        if line != "" do
+          [list_1_item, list_2_item] =
+            line
+            |> String.split("   ", trim: true)
+            |> Enum.map(&String.to_integer/1)
+
+          [[list_1_item | list_1_acc], [list_2_item | list_2_acc]]
+        else
+          [list_1_sorted, list_2_sorted] =
+            Enum.map(separated_lists, &Enum.sort/1)
+
+          _total_distance =
+            Enum.zip_reduce(list_1_sorted, list_2_sorted, 0, fn a, b, total_distance ->
+              total_distance + abs(a - b)
+            end)
+        end
+      end
+    )
   end
 end
--- test/a24_test.exs
+++ test/a24_test.exs
@@ -1,8 +1,11 @@
 defmodule A24Test do
   use ExUnit.Case
-  doctest A24
 
-  test "greets the world" do
-    assert A24.hello() == :world
+  test "day 1 pt 2" do
+    assert A24.day1pt2() == 26_859_182
+  end
+
+  test "day 1" do
+    assert A24.day1() == 1_320_851
   end
 end
